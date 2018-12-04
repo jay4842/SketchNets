316bd766898927692efb2fe0084f0ece3bfd1906
Jimmy Johnson <jay4842@gmail.com>
2018-12-04 15:42:50 -0600

    working on adding sketch nets
    Need to define networks 2 through 6

--- src/net/network_defs.py
+++ src/net/network_defs.py
@@ -5,13 +5,13 @@ import tensorflow as tf
 import numpy as np
 import src.util.model_helpers as mh
 
-def ResNet_20(cfgs, inputs, image_size, train=True):
+def ResNet_20(cfgs, inputs, image_size, train=True, transform_done=False):
     wave_type = cfgs['model']['wavelet']
     data_type = cfgs['data']['type']
     pooling = cfgs['model']['pooling']
 
     with tf.variable_scope('ResNet'):
-        if(data_type == 'MNIST'):
+        if(data_type == 'MNIST' and not transform_done):
             # we need to reshape our inputs
             inputs = tf.reshape(inputs, [-1,28,28,1])
             inputs = tf.image.grayscale_to_rgb(inputs)
@@ -42,14 +42,14 @@ def ResNet_20(cfgs, inputs, image_size, train=True):
             print(pool_out.get_shape().as_list())
             return pool_out
 
-def ResNet_18(cfgs, inputs, image_size, train=True):
+def ResNet_18(cfgs, inputs, image_size, train=True,transform_done=False):
     print('RESNET_18')
     wave_type = cfgs['model']['wavelet']
     data_type = cfgs['data']['type']
     pooling = cfgs['model']['pooling']
 
     with tf.variable_scope('ResNet'):
-        if(data_type == 'MNIST'):
+        if(data_type == 'MNIST' and not transform_done):
             # we need to reshape our inputs
             inputs = tf.reshape(inputs, [-1,28,28,1])
             inputs = tf.image.grayscale_to_rgb(inputs)
@@ -89,23 +89,15 @@ def ResNet_18(cfgs, inputs, image_size, train=True):
 '''
 From: https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/slim/python/slim/nets/alexnet.py
 
-      net = layers.conv2d(inputs, 64, [11, 11], 1, padding='VALID', scope='conv1') 64 ->  64
-      net = layers_lib.max_pool2d(net, [3, 3], 2, scope='pool1')                   64 -> 32
-      net = layers.conv2d(net, 192, [5, 5], scope='conv2')
-      net = layers_lib.max_pool2d(net, [3, 3], 2, scope='pool2')                   32 -> 16
-      net = layers.conv2d(net, 384, [3, 3], scope='conv3') 
-      net = layers.conv2d(net, 384, [3, 3], scope='conv4')
-      net = layers.conv2d(net, 256, [3, 3], scope='conv5')
-      net = layers_lib.max_pool2d(net, [3, 3], 2, scope='pool5')                   16 -> 8
 ''' 
 # Modified the initial convolution, the stride of 4 is too much, changed to 2
 # - also might cut features in half, not sure yet
-def AlexNet(cfgs, inputs, image_size):
+def AlexNet(cfgs, inputs, image_size, transform_done=False):
     with tf.variable_scope('AlexNet'):
         wave_type = cfgs['model']['wavelet']
         data_type = cfgs['data']['type']
         pooling = cfgs['model']['pooling']
-        if(data_type == 'MNIST'):
+        if(data_type == 'MNIST' and not transform_done):
             # we need to reshape our inputs
             inputs = tf.reshape(inputs, [-1,28,28,1])
             inputs = tf.image.grayscale_to_rgb(inputs)
@@ -127,6 +119,37 @@ def AlexNet(cfgs, inputs, image_size):
 
 # sketch nets now
 
+# Sketch net 1
+# - first create dwt by feeding the inputs tensor to a dwt transform
+#   - This will be based on: wave_type.
+# - Feed the four outputs into a cnn. 
+# - Return that output.
+def sketch_net_1(cfgs, inputs, image_size):
+    with tf.variable_scope('Sketch_Net_1'):
+        wave_type = cfgs['model']['wavelet']
+        data_type = cfgs['data']['type']
+        pooling = cfgs['model']['pooling']
+        # return a backend based on the dataset that is currently being used
+        cnn_backend = get_model_function(data_type, 'base')
+        # convert mnist data to d2 image vs 1d image
+        with tf.variable_scope('process'):
+            if(data_type == 'MNIST'):
+                # we need to reshape our inputs
+                inputs = tf.reshape(inputs, [-1,28,28,1])
+                inputs = tf.image.grayscale_to_rgb(inputs)
+                inputs = tf.image.resize_nearest_neighbor(inputs, (image_size[1], image_size[0]))
+            #
+            ll, lh, hl, hh = mh.dwt(inputs, wave_type, name='dwt')
+            inputs = tf.concat([ll, lh, hl, hh], axis=1)
+        # return the output of the cnn backend, 
+        # additionally we have also performed the image transform on 
+        # MNIST so we do not need to do it again.
+        return cnn_backend(cfgs, inputs, image_size, transform_done=True)
+
+# sketch net 2
+# discription
+def sketch_net_2(cfgs, inputs, image_size):
+    print('wip')
 # helper to get network def based on string input
 # - will add the other models as I go along
 def get_model_function(data_type, model_type):
